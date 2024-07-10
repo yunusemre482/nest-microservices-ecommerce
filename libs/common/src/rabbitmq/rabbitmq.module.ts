a7d4600a0d7a21f9720c705a7d9bc35ef96da712
Yunus emre akgün <akgunyunus482@gmail.com>
2024-07-10 21:21:47 +0300

    chore: Add Redis module to common library

---
+++ libs/common/src/rabbitmq/rabbitmq.module.ts
@@ -0,0 +1,36 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { RabbitMQService } from './rabbitmq.service';
+
+interface RmqModuleOptions {
+  name: string;
+}
+
+@Module({
+  providers: [RabbitMQService],
+  exports: [RabbitMQService],
+})
+export class RabbitMqModule {
+  static register({ name }: RmqModuleOptions): DynamicModule {
+    return {
+      module: RabbitMqModule,
+      imports: [
+        ClientsModule.registerAsync([
+          {
+            name,
+            useFactory: (configService: ConfigService) => ({
+              transport: Transport.RMQ,
+              options: {
+                urls: [configService.get<string>('RABBIT_MQ_URI') ?? 'amqp://localhost:5672'],
+                queue: configService.get<string>(`RABBIT_MQ_${name}_QUEUE`),
+              },
+            }),
+            inject: [ConfigService],
+          },
+        ]),
+      ],
+      exports: [ClientsModule],
+    };
+  }
+}
