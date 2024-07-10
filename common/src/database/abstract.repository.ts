dc4a28e6ac2c6ff4d551a4924ae46f91cb7b7e45
Yunus emre akgün <akgunyunus482@gmail.com>
2024-07-11 00:29:56 +0300

    chore: Add Redis module to common library

---
+++ common/src/database/abstract.repository.ts
@@ -0,0 +1,92 @@
+import { Logger, NotFoundException } from '@nestjs/common';
+import {
+  FilterQuery,
+  Model,
+  Types,
+  UpdateQuery,
+  SaveOptions,
+  Connection,
+  PipelineStage,
+} from 'mongoose';
+import { AbstractDocument } from './abstract.schema';
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(
+    protected readonly model: Model<TDocument>,
+    private readonly connection: Connection,
+  ) { }
+
+  async create(
+    document: Omit<TDocument, '_id'>,
+    options?: SaveOptions,
+  ): Promise<TDocument> {
+
+    const createdDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+
+    const savedDocuments = await createdDocument.save(options)
+
+    return savedDocuments.toJSON() as unknown as TDocument;
+  }
+
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<Partial<TDocument>> {
+    const document = await this.model.findOne(filterQuery, {}, { lean: true });
+
+    if (!document) {
+      this.logger.warn('Document not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document not found.');
+    }
+
+    return document as Partial<TDocument>;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>,
+  ) {
+    const document = await this.model.findOneAndUpdate(filterQuery, update, {
+      lean: true,
+      new: true,
+    });
+
+    if (!document) {
+      this.logger.warn(`Document not found with filterQuery:`, filterQuery);
+      throw new NotFoundException('Document not found.');
+    }
+
+    return document;
+  }
+
+  async upsert(
+    filterQuery: FilterQuery<TDocument>,
+    document: Partial<TDocument>,
+  ) {
+    return this.model.findOneAndUpdate(filterQuery, document, {
+      lean: true,
+      upsert: true,
+      new: true,
+    });
+  }
+
+  async find(filterQuery: FilterQuery<TDocument>) {
+    return this.model.find(filterQuery, {}, { lean: true });
+  }
+
+  async aggregate(pipeline: PipelineStage[]): Promise<TDocument | undefined> {
+    return await this.model.aggregate(pipeline) as unknown as TDocument;
+  }
+
+  async countDocuments(filterQuery: FilterQuery<TDocument>) {
+    return await this.model.countDocuments(filterQuery);
+  }
+
+  async startTransaction() {
+    const session = await this.connection.startSession();
+    session.startTransaction();
+    return session;
+  }
+}
---
+++ common/src/index.ts
@@ -0,0 +1,8 @@
+export * from './lib/common.module';
+
+export * from './rabbitmq/rabbitmq.service'
+export * from './rabbitmq/rabbitmq.module'
+
+export * from './database/abstract.repository'
+export * from './database/abstract.schema'
+export * from './database/database.module'
